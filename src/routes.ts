db12bb54a4a59e45bab708b1530d1365edb1b9ea
Kelvinsbs <nivlek.barcelos@gmail.com>
2021-10-25 22:56:01 -0300

    commit do dia 25/10/2021

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ensureAuthenticate } from "./middlewares/ensureAuthenticate";
 
 const router = Router();
 
@@ -12,10 +13,9 @@ const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new  CreateComplimentController();
 
-router.post("/users", createUserController.handle);
-
 // router.use(ensureAdmin) //Se for feito dessa forma, todas as rotas abaixo dessa linha, obrigatoriamente irão passar pelo middware, e sera feita a validação
-router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/tags", ensureAuthenticate, ensureAdmin, createTagController.handle);
+router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments", createComplimentController.handle);
 
