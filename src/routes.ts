db2b0b49d15dc3e4d34e9672dde7f3690b247f75
Kelvinsbs <nivlek.barcelos@gmail.com>
2021-10-25 21:47:17 -0300

    aula 4 finalizada nlwValoriza

--- src/routes.ts
+++ src/routes.ts
@@ -3,17 +3,20 @@ import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new  CreateComplimentController();
 
 router.post("/users", createUserController.handle);
 
 // router.use(ensureAdmin) //Se for feito dessa forma, todas as rotas abaixo dessa linha, obrigatoriamente irão passar pelo middware, e sera feita a validação
 router.post("/tags", ensureAdmin, createTagController.handle);
-router.post("/login", authenticateUserController.handle)
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export { router } 
